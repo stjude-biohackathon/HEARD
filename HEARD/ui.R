cc8b04c902ea64972b9fbae8a8f3dd1d6a082654
Daniel Darnell <Daniel.darnell1986@gmail.com>
2022-04-12 14:36:03 -0500

    Update ui.R

--- HEARD/ui.R
+++ HEARD/ui.R
@@ -24,11 +24,11 @@ gene_loh <- read.table(file = "./gene_LOH_events.txt",header = TRUE)
 
 #EVAN
 
-clinical_data <- read.table(file="../TCGA_HRD_positive_samples_mut_calls.txt", sep="\t",header=TRUE)
+clinical_data <- read.table(file="./TCGA_HRD_positive_samples_mut_calls.txt", sep="\t",header=TRUE)
 hrd_data <- read.table(file="./TCGA_HRD_positive_samples.txt", sep="\t", header=TRUE)
 id_data<-read.table(file="./TCGA_ID_signature_exposures.txt", sep="\t", header=TRUE)
 sbs_data <- read.table(file="./TCGA_SBS_signature_exposure.txt", sep="\t", header=TRUE)
-mut_call_data <- read.table(file="../TCGA_HRD_positive_samples_mut_calls.txt", sep="\t", header=TRUE)
+mut_call_data <- read.table(file="./TCGA_HRD_positive_samples_mut_calls.txt", sep="\t", header=TRUE)
 
 
 #EVAN
@@ -64,63 +64,63 @@ navbarPage(
   "HEARD",
   img(
     src = "images/HEARD.png",
-    width = "150px"
+    width = "200px"
   ),
-  tabPanel("Patient_View",
+  tabPanel("Patient View",
            fluidRow(sidebarPanel(width=2,
-                        # naming this panel the Case Navigator
-                        h3("Case Navigator"),
-                        # get patient ID info for rendering
-                        pickerInput("pat_id", "Patient:", 
-                                    choices=(sbs_data$FileName)),
-                        # Radio button for report
-                        radioButtons("hrd_stat","HRD Positive",choices=c("Yes","No")),
-                        # button for downloading the report
-                        h3("Click to Generate Report"),
-                        downloadButton("report", "Generate report"),
+                                 # naming this panel the Case Navigator
+                                 h3("Case Navigator"),
+                                 # get patient ID info for rendering
+                                 pickerInput("pat_id", "Patient:", 
+                                             choices=(sbs_data$FileName)),
+                                 # Radio button for report
+                                 radioButtons("hrd_stat","HRD Positive",choices=c("Yes","No")),
+                                 # button for downloading the report
+                                 h3("Click to Generate Report"),
+                                 downloadButton("report", "Generate report"),
            ),
-                                # First panel is the patient data - NEED TO RENDER
-                              
-                                  column(
-                                    width=3,offset=0,
-                                    h3("Patient Information"),
-                                    #withLoader(textOutput("clinical_info")),
-                                    htmlOutput("clinical_info")           
-                                  ), # column end
-                          
-                                
-                                  # FMove to a single row
-                                  column(width=3,#offset=2,
-                                         h3("HRD Score Metrics"),
-                                         pickerInput("hrd_metrics", "HRD Metric:", 
-                                                     choices=colnames(hrd_data[,-1])),
-                                         withLoader(plotOutput("hrdScores", height="400px", width="400px"),
-                                                    type="html", loader="pacman")
-                                  ), #end hrdScores column
-                                  column(width=3,#offset=4,
-                                         h3("Mutational Signature Exposure"),
-                                         withLoader(plotOutput("mutsigs", height="400px", width="400px"),
-                                                    type="html", loader="pacman")
-                                  
-                                ),
+           # First panel is the patient data - NEED TO RENDER
+           
+           column(
+             width=3,offset=0,
+             h3("Patient Information"),
+             #withLoader(textOutput("clinical_info")),
+             htmlOutput("clinical_info")           
+           ), # column end
+           
+           
+           # FMove to a single row
+           column(width=3,#offset=2,
+                  h3("HRD Score Metrics"),
+                  pickerInput("hrd_metrics", "HRD Metric:", 
+                              choices=colnames(hrd_data[,-1])),
+                  withLoader(plotOutput("hrdScores", height="400px", width="400px"),
+                             type="html", loader="dnaspin")
+           ), #end hrdScores column
+           column(width=3,#offset=4,
+                  h3("Mutational Signature Exposure"),
+                  withLoader(plotOutput("mutsigs", height="400px", width="400px"),
+                             type="html", loader="dnaspin")
+                  
            ),
-                                # next Row is the chromosome info
-                                fluidRow(
-                                  column(width=2,offset=7,
-                                         pickerInput("Chromosome", "Chromosome:",
-                                                     choices = c('chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13',
-                                                                 'chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX'))),
-                                ),
-                                fluidRow(
-                                  column(width=12, offset=2,
-                                         withLoader(plotOutput("chromImage", height="300px", width="300px"),
-                                                    type="html", loader="pacman")
-                                  ), #end chromImage column
-                                ),
-                                #), #end fluidRow2
-                                #),#end fluidrow1
-                       ),
-  tabPanel("Cohort_View",
+           ),
+           # next Row is the chromosome info
+           fluidRow(
+             column(width=2,offset=7,
+                    pickerInput("Chromosome", "Chromosome:",
+                                choices = c('chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13',
+                                            'chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX'))),
+           ),
+           fluidRow(
+             column(width=12, offset=2,
+                    withLoader(plotOutput("chromImage", height="300px", width="300px"),
+                               type="html", loader="dnaspin")
+             ), #end chromImage column
+           ),
+           #), #end fluidRow2
+           #),#end fluidrow1
+  ),
+  tabPanel("Cohort View",
            sidebarPanel(pickerInput("TCGA", "TCGA:", 
                                     choices = (tcga_data[,1])),
                         pickerInput("Column", "Column:",
@@ -131,7 +131,7 @@ navbarPage(
            fluidRow(
              column(
                width=12,
-               withLoader(plotOutput("LOH_Info"), type="html", loader="pacman")
+               withLoader(plotOutput("LOH_Info"), type="html", loader="dnaspin")
              )
            ),
            fluidRow(
@@ -154,7 +154,8 @@ navbarPage(
   tabPanel(
     "Upload Data Table",
     fluidPage(
-      wellPanel(p("Upload your data file below and click Analyze Data to begin visualization.")),
+      wellPanel(p("Upload your data file below and click Analyze Data to begin visualization."),
+                "Allowed filetypes are .csv, .tsv, .txt, and .xlsx"),
       fluidRow(
         column(fileInput("uploaded_HRD_data",
                          label = "Upload your data",
@@ -163,7 +164,7 @@ navbarPage(
       ),
       actionButton("anaylze_data",
                    "Analyze Data"),
-      withLoader(dataTableOutput("dt_PatientData", width = "100%", height = "auto"), type="html", loader="pacman")
+      withLoader(dataTableOutput("dt_PatientData", width = "100%", height = "auto"), type="html", loader="dnaspin")
     )
   ),
   
@@ -171,4 +172,3 @@ navbarPage(
   
   
 )
-
