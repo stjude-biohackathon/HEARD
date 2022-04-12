9f6c5935e3ce0985d636b200b22cda7702919930
Daniel Darnell <Daniel.darnell1986@gmail.com>
2022-04-12 11:01:57 -0500

    Update ui.R

--- HEARD/ui.R
+++ HEARD/ui.R
@@ -63,7 +63,8 @@ navbarPage(
   tabPanel(
     "Upload Data Table",
     fluidPage(
-      wellPanel(p("Upload your data file below and click Analyze Data to begin visualization.")),
+      wellPanel(p("Upload your data file below and click Analyze Data to begin visualization."),
+                "Allowed filetypes are .txt, .csv, .tsv, and .xlsx"),
       fluidRow(
         column(fileInput("uploaded_HRD_data",
                          label = "Upload your data",
